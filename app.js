0fd077c2c982ecca2dc796a8d652462bec5cd778
MiaBrunner <mbrunner.mktg@gmail.com>
2020-07-11 11:03:18 +1000

    added auth route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const quoteRouter = require("./routes/quotes_routes")
+const authRouter = require('./routes/auth_routes');
 
 const port = 3000
 
@@ -9,7 +10,9 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+// Define routes 
 app.use("/quotes", quoteRouter)
+app.use("/auth", authRouter)
 
 app.listen(port, () => {
 	console.log(`Electrician app listening on port ${port}`)
